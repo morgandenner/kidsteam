e268d7a749471f8f2dd4ae0250f013854461788f
Morgan Denner <morgandenner@yahoo.com>
2015-04-19 17:48:16 -0400

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "kt");    // The database username. 
+define("PASSWORD", "ktpassword");    // The database password. 
+define("DATABASE", "kidsteam");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
